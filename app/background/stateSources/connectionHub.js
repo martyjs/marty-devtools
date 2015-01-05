d6ffc16d14f87ef4a948e812cac9e9d08d3a46fd
James Hollingworth <jamiehollingworth@gmail.com>
2015-01-05 07:48:48 +0000

    refactored connections

---
+++ app/background/stateSources/connectionHub.js
@@ -0,0 +1,95 @@
+var connections = {};
+var MESSAGE = 'message';
+var _ = require('underscore');
+var EventEmitter = require('events').EventEmitter;
+
+var Hub = {
+  devtools: new DevtoolsPageConnection(),
+  inspectedWindow: new InspectedWindowConnection()
+};
+
+module.exports = Hub;
+
+function DevtoolsPageConnection() {
+  var events = new EventEmitter();
+
+  this.send = send;
+  this.onMessage = onMessage;
+
+  chrome.runtime.onConnect.addListener(onConnect);
+
+  function send(tabId, message) {
+    if (tabId in connections) {
+      connections[tabId].postMessage(message);
+    }
+  }
+
+  function onMessage(cb, context) {
+    if (context) {
+      cb = _.bind(cb, context);
+    }
+
+    events.on(MESSAGE, cb);
+
+    return {
+      dispose: function () {
+        events.removeListener(MESSAGE, cb);
+      }
+    };
+  }
+
+  function onConnect(port) {
+    port.onDisconnect.addListener(onDisconnect);
+    port.onMessage.addListener(_.partial(onMessageFromDevtools, port));
+  }
+
+  function onMessageFromDevtools(port, message) {
+    if (message.type === 'init') {
+      connections[message.tabId] = port;
+    } else if (!sender.tab) {
+      emitter.emit(MESSAGE, message);
+    }
+  }
+
+  function onDisconnect(port) {
+    port.onMessage.removeListener(onMessage);
+    removePort(port);
+  }
+
+  function removePort(port) {
+    _.each(connections, function (value, key) {
+      if (port === value) {
+        delete connections[value];
+      }
+    });
+  }
+}
+
+function InspectedWindowConnection() {
+  var events = new EventEmitter();
+
+  this.onMessage = onMessage;
+  chrome.runtime.onMessage.addListener(onMessageFromInspectedWindow);
+
+  function onMessage(cb, context) {
+    if (context) {
+      cb = _.bind(cb, context);
+    }
+
+    events.on(MESSAGE, cb);
+
+    return {
+      dispose: function () {
+        events.removeListener(MESSAGE, cb);
+      }
+    };
+  }
+
+  function onMessageFromInspectedWindow(request, sender) {
+    if (sender.tab) {
+      events.emit(MESSAGE, sender.tab.id, request);
+    }
+
+    return true;
+  }
+}
